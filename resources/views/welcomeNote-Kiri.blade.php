1ec51f882c453f75fd771c563df370fab85887bb
kevin-kobayashi <kx7787@gmail.com>
2023-07-13 02:04:08 +0900

    "auth init & NewwelcomePage"

---
+++ resources/views/welcomeNote-Kiri.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container d-flex flex-column justify-content-center align-items-center vh-100 bg-gray-50 dark:bg-gray-800">
+    <div class="w-100 text-center">
+        <div class="mb-2">Welcome to NoteKiri</div>
+        <div class="mb-4">Log in with your account to continue</div>
+    </div>
+</div>
+
+@endsection
