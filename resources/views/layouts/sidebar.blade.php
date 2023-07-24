732da47f253adbb8bb72bbee89fd978d40a2efd6
kevin-kobayashi <kx7787@gmail.com>
2023-07-24 10:22:52 +0900

    "color change"

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,17 +4,17 @@
             <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start overflow-y-auto" style="max-height: 80vh;">
                 @foreach ($latestArticles as $article)
                     <a href="{{ route('articles.show', $article->id) }}" class="list-group-item border-end-0 d-inline-block text-truncate bg-dark bg-gradient 
-                    @if(request()->is('articles/' . $article->id)) active list-group-item-primary @endif" 
+                    @if(request()->is('articles/' . $article->id)) active list-group-item-danger @endif" 
                         data-bs-parent="#sidebar">{{ $article->title }}
                     </a>
                 @endforeach
             </div>
             <div class="dropdown-center">
                 <button class="w-100 btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    <i class="me-1 bi bi-person-circle"></i>{{$username}}
+                    <i class="bi bi-person-circle"></i> {{$username}}
                 </button>
                 <ul class="dropdown-menu dropdown-menu-dark">
-                    <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a></li>
+                    <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="bi bi-box-arrow-right"></i> {{ __('Logout') }}</a></li>
                     <li><hr class="dropdown-divider border-danger"></li>
                     <li><a class="dropdown-item" href="#">Action two</a></li>
                     <li><a class="dropdown-item" href="#">Action three</a></li>
