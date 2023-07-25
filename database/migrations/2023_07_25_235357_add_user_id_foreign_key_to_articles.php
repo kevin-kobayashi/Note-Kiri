74e02d0c4da981f6b8e2e79b3fa0a38cbc1cda75
kevin-kobayashi <kx7787@gmail.com>
2023-07-26 03:21:19 +0900

    "Adding user_id and explicit external constraints"

---
+++ database/migrations/2023_07_25_235357_add_user_id_foreign_key_to_articles.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+
+            // 外部キー制約を追加
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+
+            // マイグレーションをロールバックする場合は外部キー制約を削除
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+    }
+};
