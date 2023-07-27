edf4c7645db8efaf2b7827b37749244a96f83295
kevin-kobayashi <kx7787@gmail.com>
2023-07-28 06:48:45 +0900

    "slot html"

---
+++ resources/views/tools/slot.blade.php
@@ -0,0 +1,46 @@
+<div class="slot" id="slotArea">
+    <div class="slot-frame">
+        <ul class="reels" style="list-style: none;">
+            <li class="reel"><img src="{{ asset('img/bell-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/x-diamond-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+            <li class="reel"><i class="bi bi-star-half" aria-hidden="true"></i></li>
+            <li class="reel"><i class="bi bi-sun-fill" aria-hidden="true"></i></li>
+            <li class="reel"><img src="{{ asset('img/7-circle.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/bell-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/x-diamond-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+        </ul>
+        <ul class="reels" style="list-style: none;">
+            <li class="reel"><img src="{{ asset('img/x-diamond-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+            <li class="reel"><i class="bi bi-star-half" aria-hidden="true"></i></li>
+            <li class="reel"><i class="bi bi-sun-fill" aria-hidden="true"></i></li>
+            <li class="reel"><img src="{{ asset('img/bell-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/7-circle.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/x-diamond-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+            <li class="reel"><i class="bi bi-star-half" aria-hidden="true"></i></li>
+        </ul>
+        <ul class="reels" style="list-style: none;">
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+            <li class="reel"><i class="bi bi-star-half" aria-hidden="true"></i></li>
+            <li class="reel"><i class="bi bi-sun-fill" aria-hidden="true"></i></li>
+            <li class="reel"><img src="{{ asset('img/bell-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/x-diamond-fill.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/7-circle.svg') }}" alt=""></li>
+            <li class="reel"><img src="{{ asset('img/suit-heart-fill.svg') }}" alt=""></li>
+            <li class="reel"><i class="bi bi-star-half" aria-hidden="true"></i></li>
+            <li class="reel"><i class="bi bi-sun-fill" aria-hidden="true"></i></li>
+        </ul>
+    </div>
+</div>
+<div>
+    <button type="button" class="btn-start">start</button>
+    <button type="button" class="btn-reset" disabled="true">reset</button>
+</div>
+<div>
+    <button type="button" class="btn-stop" data-val="0" disabled="true">stop 0</button>
+    <button type="button" class="btn-stop" data-val="1" disabled="true">stop 1</button>
+    <button type="button" class="btn-stop" data-val="2" disabled="true">stop 2</button>
+</div>
