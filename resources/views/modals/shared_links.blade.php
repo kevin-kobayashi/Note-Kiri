5e454e6934b735dbbf658fd1e3ea168547be699e
kevin-kobayashi <kx7787@gmail.com>
2023-07-30 03:11:24 +0900

    "sharedLinksModal"

---
+++ resources/views/modals/shared_links.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="sharedLinksModal" tabindex="-1" aria-labelledby="sharedLinksModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="sharedLinksModalLabel">共有リンク一覧</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <ul id="sharedLinksList">
+          <!-- 共有リンク一覧がここに表示されます -->
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">閉じる</button>
+      </div>
+    </div>
+  </div>
+</div>
