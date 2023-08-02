fce82d778a61d3641229e3156337c574ef9efa57
kevin-kobayashi <kx7787@gmail.com>
2023-08-02 15:40:32 +0900

    "Easy to read code"

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,17 @@
 <div class="col-auto px-0 pt-2" style="max-width: 150px;">
     <div id="sidebar" class="collapse collapse-horizontal show border-end border-bottom">
         <nav class="d-flex flex-column">
+
             <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start overflow-y-auto" style="max-height: 80vh;">
+            
                 @foreach ($latestArticles as $article)
                     <a href="{{ route('articles.show', $article->id) }}" class="list-group-item border-end-0 d-inline-block text-truncate bg-dark bg-gradient 
-                    @if(request()->is('articles/' . $article->id)) active list-group-item-danger @endif" 
+                        @if(request()->is('articles/' . $article->id)) active list-group-item-danger @endif" 
                         data-bs-parent="#sidebar">{{ $article->title }}
                     </a>
                 @endforeach
             </div>
+            
             <div class="dropdown-center">
                 <button class="w-100 btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="bi bi-person-circle"></i> {{$username}}
