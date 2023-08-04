d35e087e831137fdd3270a2dff7b7406060397bf
kevin-kobayashi <kx7787@gmail.com>
2023-08-05 00:42:27 +0900

    "offcanvas sidebar"

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,17 +1,18 @@
-<div class="col-auto px-0 pt-2" style="max-width: 150px;">
-    <div id="sidebar" class="collapse collapse-horizontal show border-end border-bottom">
+<div class="col-auto px-0 pt-2">
+    
+    <div id="sidebar" class="offcanvas offcanvas-start overflow-y-auto" data-bs-theme="dark" data-bs-backdrop="true" tabindex="-1" aria-labelledby="sidebarLabel">
         <nav class="d-flex flex-column">
-
-            <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start overflow-y-auto" style="max-height: 80vh;">
-            
-                @foreach ($latestArticles as $article)
-                    <a href="{{ route('articles.show', $article->id) }}" class="list-group-item border-end-0 d-inline-block text-truncate bg-dark bg-gradient 
-                        @if(request()->is('articles/' . $article->id)) active list-group-item-danger @endif" 
-                        data-bs-parent="#sidebar">{{ $article->title }}
-                    </a>
-                @endforeach
+            <div class="offcanvas-body">
+                <!-- サイドバーのトグルボタン -->
+                <button data-bs-dismiss="offcanvas" class="border rounded-3 p-1 text-decoration-none mb-3"><i class="bi bi-caret-left-fill"></i> Menu</button>
+                <div id="sidebar-nav" class="list-group border-0 rounded-0 text-sm-start " style="max-height: 80vh;">
+                    @foreach ($latestArticles as $article)
+                        <a href="{{ route('articles.show', $article->id) }}" class="list-group-item border-end-0 d-inline-block text-truncate bg-dark bg-gradient @if(request()->is('articles/' . $article->id)) active list-group-item-danger text-primary @endif" data-bs-parent="#sidebar">{{ $article->title }}</a>
+                    @endforeach
+                </div>
             </div>
             
+            <!-- ドロップダウンメニュー -->
             <div class="dropdown-center">
                 <button class="w-100 btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="bi bi-person-circle"></i> {{$username}}
@@ -25,11 +26,15 @@
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                 @csrf
             </form>
+
+            <!-- サイコロを振るツール -->
             @component('tools.dice_roll')
             @endcomponent
+
+            <!-- スロットを表示するツール -->
             @component('tools.slot')
             @endcomponent
+            
         </nav>
     </div>
 </div>
-@include('modals.settings')
