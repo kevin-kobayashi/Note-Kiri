954733298e1d94f555698b37ffe9913419d7da0f
kevin-kobayashi <kx7787@gmail.com>
2023-08-05 19:07:17 +0900

    "Buttons and text-break"

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,17 +2,14 @@
 
 @section('content')
 <div class="container-fluid">
-    <div class="row flex-nowrap">
+    <div class="row">
         @component('layouts.sidebar')
         @endcomponent
-        <main class="col ps-md-2 pt-2">
-
-            <!-- サイドバーのトグルボタン -->
-            <button href="#" data-bs-target="#sidebar" data-bs-toggle="offcanvas" class="btn position-fixed border rounded-3 p-1 text-decoration-none"><i class="bi bi-list bi-lg py-2 p-1"></i> Menu</button>
-
-            <div class="mx-auto w-100 p-4">
-                <div class="mb-1 border-bottom border-secondary pt-3">
-                    <h1 class="fw-bolder">{{ $article->title }}</h1>
+        <main class="ps-md-2 pt-2">
+            @component('layouts.header_buttons')
+            @endcomponent
+            <div class="mx-auto p-1">
+                <div class="border-bottom border-secondary pt-3">
                     <div class="d-flex pt-1 align-items-baseline">
                         <div>{{$article->updated_at->format('m d, Y')}}</div>
                         <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="ms-auto"><i class="bi fs-1 bi-pencil-square"></i></a>
@@ -25,10 +22,8 @@
                     </div>
                 </div>
             </div>
-            <div class="flax p-4 gap-4 align-baseline m-auto">
-                <div class="flex flex-grow flex-col gap-3">
-                    <div>{!! nl2br(e($article->content)) !!}</div>
-                </div>
+            <div class="p-4">
+                <p class="text-break">{!! nl2br(e($article->content)) !!}</p>
             </div>
             <!-- 新規投稿作成ボタン -->
             <div class="position-fixed bottom-0 end-0 mb-5 me-4">
