abd94fb325eb519bc7e6549024d6964a90eb40fc
kevin-kobayashi <kx7787@gmail.com>
2023-08-06 15:22:43 +0900

    "Logo and adjusted layout"

--- resources/views/shared_links/show.blade.php
+++ resources/views/shared_links/show.blade.php
@@ -2,15 +2,20 @@
 
 @section('content')
 <div class="container-fluid">
+    @component('layouts.header')
+    @endcomponent
     <div class="row flex-nowrap">
         <main class="col ps-md-2 pt-2">
-            <div class="mx-auto w-100 p-4">
+            <div class="mx-auto w-100 p-2">
                 <div class="mb-1 border-bottom border-secondary pt-3">
                     <h1 class="fw-bolder text-break">{{ $article->title }}</h1>
                     <div class="d-flex pt-1 align-items-baseline">
-                        <div>{{$article->updated_at->format('m d, Y')}}</div>
-                        <!-- フォームのURL出力欄 -->
-                        <p class="ms-2">{{__('shared link:')}}</p><input class="text-info-emphasis" type="text" name="url" value="{{ $sharedLink }}" readonly>
+                        
+                            <div>{{$article->updated_at->format('m d, Y')}}</div>
+                            <!-- フォームのURL出力欄 -->
+                            <div class="ms-2">{{__('shared link:')}}</div>
+                            <input class="text-info-emphasis" type="text" name="url" value="{{ $sharedLink }}" readonly>
+                        
                     </div>
                 </div>
             </div>
