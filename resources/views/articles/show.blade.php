c13568be0036618bc51f3a49a0a3b378fa4b3f21
kevin-kobayashi <kx7787@gmail.com>
2023-08-07 19:50:59 +0900

    "Adjusted layout"

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -12,13 +12,13 @@
                 <div class="border-bottom border-secondary pt-3">
                     <div class="d-flex pt-1 align-items-baseline">
                         <div>{{$article->updated_at->format('m d, Y')}}</div>
+
                         <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="ms-auto"><i class="bi fs-1 bi-pencil-square"></i></a>
                         <form action="{{ route('articles.share', ['id' => $article->id]) }}" method="post" target="_blank">
                             @csrf
-                            <button type="submit" class="ms-2 bg-transparent"><i class="bi fs-1 bi-share"></i></button>
+                            <button type="submit" class="ms-3 bg-transparent"><i class="bi fs-1 bi-share"></i></button>
                         </form>
-                        
-                        <a class="ms-2" href="#" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $article->id }}"><i class="bi fs-1 bi-trash"></i></a>
+                        <a class="ms-3" href="#" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $article->id }}"><i class="bi fs-1 bi-trash"></i></a>
                     </div>
                 </div>
             </div>
