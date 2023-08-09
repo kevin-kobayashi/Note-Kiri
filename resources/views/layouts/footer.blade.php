833aa00852534b533eda5fabf4e77e6ec2c2a369
kevin-kobayashi <kx7787@gmail.com>
2023-08-10 00:13:54 +0900

    "style Better looking footer"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,9 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm fixed-bottom mt-5">
-    <a class="navbar-brand mx-auto" href="{{ route('articles.index') }}">
-        <img src="{{ asset('img/KIei-a1.png') }}" class="img-fluid" alt="logo">
-    </a>
-</nav>
+<footer>
+    <div class="d-flex flex-column navbar bg-secondary shadow-sm fixed-bottom mt-5">
+        <a class="navbar-brand rounded-3" href="#">
+            <img src="{{ asset('img/KIei-a1.png') }}" class="img-fluid" alt="logo">
+        </a>
+        <p>&copy; 2023-{{ date('Y') }} Kevin Kuroda</p>
+    </div>
+</footer>
+
