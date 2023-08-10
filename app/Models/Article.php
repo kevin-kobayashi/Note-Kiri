a8354600960f7c541ff4281b8dd7a47063a5bf87
kevin-kobayashi <kx7787@gmail.com>
2023-08-11 00:53:59 +0900

    "style
    Remove boot method to remove commented out related elements,"

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -18,15 +18,4 @@ class Article extends Model
     {
         return $this->hasOne('App\Models\SharedLink');
     }
-
-
-    public static function boot()
-    {
-        parent::boot();
-        // 削除された記事に関連する共有リンクを削除
-        static::deleting(function ($article) {
-            $article->shared_link()->delete();
-        });
-
-    }
 }
