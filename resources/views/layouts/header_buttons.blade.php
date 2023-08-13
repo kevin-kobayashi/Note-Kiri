dd8cf28d174b52249b470db8be8473f466a3fc45
kevin-kobayashi <kx7787@gmail.com>
2023-08-13 21:10:10 +0900

    "style
    Sticky button on header."

--- resources/views/layouts/header_buttons.blade.php
+++ resources/views/layouts/header_buttons.blade.php
@@ -1,4 +1,4 @@
-<header class="d-flex align-items-center justify-content-between">
+<header class="d-flex align-items-center justify-content-between sticky-top">
     <button href="#" data-bs-target="#sidebar" data-bs-toggle="offcanvas" class="bg-transparent d-flex rounded-3 p-1"><i class="fs-1 bi bi-book px-1"></i><i class="fs-1 bi bi-arrow-bar-right px-1"></i></button>
     <button class="bg-transparent rounded-3 p-1" href="#" data-bs-toggle="modal" data-bs-target="#userSettings" data-toggle="tooltip" title="{{__('Settings')}}">
         <i class="fs-1 bi bi-gear-wide-connected py-2 px-1"></i>
