b10e694c30972ff75029fd8aca344e6a8adc463e
kevin-kobayashi <kx7787@gmail.com>
2023-08-16 22:42:54 +0900

    "style
    Changed layout of delete account button before email verification"

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -16,17 +16,18 @@
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
                     <br>{{ __('If you did not receive the email') }}
-                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline" 
+                    <button type="submit" class="btn btn-link link-underline-primary p-0 m-0 border" 
                         onclick="event.preventDefault(); document.getElementById('resend-form').submit();" data-toggle="tooltip" title="再送してから少しお待ちください。">
                         {{ __('Resend email') }}
                     </button>
-
+                </div>
+                <div class="card-footer">
                     <!-- 削除ボタンを追加 -->
                     <form method="POST" action="{{ route('verification.delete') }}">
                         @csrf
-                        <button type="submit" class="btn btn-danger">{{ __('Delete Account') }}</button>
+                        <p>何らかの理由で、メール認証を行う前にアカウントを削除したい場合は、以下のボタンをご利用いただけますようお願い申し上げます。</p>
+                        <button type="submit" class="btn btn-light">{{ __('Delete Account') }}</button>
                     </form>
-
                 </div>
             </div>
         </div>
