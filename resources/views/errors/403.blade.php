473f69329098326c627212a03499119f29fb95b6
kevin-kobayashi <kx7787@gmail.com>
2023-08-16 22:43:44 +0900

    "style
    Edit text on page 403"

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -4,7 +4,7 @@
 <div class="container-fluid bg-dark text-white d-flex align-items-center justify-content-center vh-100">
     <div class="text-center">
         <h1 class="display-4 mb-4">403 Forbidden</h1>
-        <p class="lead mb-5">Access to this area is restricted.</p>
+        <p class="lead mb-5">Access to this route is restricted.</p>
         <p class="h5">Please ensure you have proper authorization.</p>
     </div>
 </div>
