822fd6a9391e5cfafeb1d009d1fe335103714c0c
kevin-kobayashi <kx7787@gmail.com>
2023-08-18 17:55:36 +0900

    "feat
    Explicitly define policy in AuthServiceProvider"

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Article;
+use App\Policies\ArticlePolicy;
+use App\Models\SharedLink;
+use App\Policies\SharedLinkPolicy;
+
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -14,6 +19,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Article::class => ArticlePolicy::class,
+        SharedLink::class => SharedLinkPolicy::class,
     ];
 
     /**
