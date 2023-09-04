4a74d859b1d160e0de1c3509fb08e038aa8bceb0
kevin-kobayashi <kx7787@gmail.com>
2023-09-04 12:44:40 +0900

    "style
    Change developer's name"

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,6 +3,6 @@
         <a class="navbar-brand rounded-3" href="#">
             <img src="{{ asset('img/KIei-a1.png') }}" class="img-fluid" alt="logo">
         </a>
-        <p>&copy; 2023-{{ date('Y') }} Kevin Kuroda</p>
+        <p>&copy; 2023-{{ date('Y') }} Kevin Kobayashi</p>
     </div>
 </footer>
